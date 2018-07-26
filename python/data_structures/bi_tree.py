686a672445e63b1f80f5d15aa51c3aa91e9f461e
leejroberts <leejamesroberts@gmail.com>
2018-07-26 09:34:16 -0400

    added remove action to BinaryTree with associated methods and tests.

--- python/data_structures/bi_tree.py
+++ python/data_structures/bi_tree.py
@@ -1,9 +1,25 @@
+from random import shuffle
 
 class Node:
     def __init__(self, value=None, left_b=None, right_b=None):
         self.left_b = left_b
         self.value = value
         self.right_b  = right_b
+
+    def has_no_children(self):
+        return (self.left_b is None and self.right_b is None)
+
+    def has_both_children(self):
+        return (self.left_b and self.right_b)
+
+    def has_any_children(self):
+        return (self.left_b or self.right_b)
+
+    def update_attrs_from(self, update_node):
+        self.left_b = update_node.left_b
+        self.value = update_node.value
+        self.right_b = update_node.right_b
+
 
 class BinaryTree:
     def __init__(self, value=None):
@@ -36,19 +52,62 @@ class BinaryTree:
 
     def search(self, value, current_node=None):
         """returns true if value present, else false"""
+        node = self.return_node(value, current_node)
+        return True if node else False
+
+
+    def return_node(self, value, current_node=None):
+        """returns the node containing the value given if not found returns False"""
         current_node = current_node or self.base
 
-        if current_node.value == value:
-            return True
-        elif current_node.value is None:
+        if current_node.value is None:
             return False
-        elif value > current_node.value:
-            if current_node.right_b is None:
-                return False
-            else:
-                return self.search(value, current_node.right_b)
+        elif value == current_node.value:
+            return current_node
+        elif value > current_node.value and current_node.right_b:
+            current_node = current_node.right_b
+            return self.return_node(value, current_node)
+        elif value < current_node.value and current_node.left_b:
+            current_node = current_node.left_b
+            return self.return_node(value, current_node)
         else:
-            if current_node.left_b is None:
-                return False
+            return False
+
+
+    def remove(self, value):
+
+        def remove_rec(starting_node, update_node):
+            if update_node.left_b is None:
+                starting_node.value = update_node.value
+                if update_node.right_b:
+                    right_node = update_node.right_b
+                    update_node.update_attrs_from(right_node)
+                    del right_node
+                return True
             else:
-                return self.search(value, current_node.left_b)
+                update_node = update_node.left_b
+                return remove_rec(starting_node, update_node)
+
+        starting_node = self.return_node(value)
+        if not starting_node: # if value was not found
+            return False
+        elif starting_node.has_no_children():
+            starting_node.value = None
+            return True
+        elif starting_node.left_b and not starting_node.right_b:
+            update_node = starting_node.left_b
+            starting_node.update_attrs_from(update_node)
+            del update_node
+            return True
+        elif starting_node.right_b:
+            update_node = starting_node.right_b
+            return remove_rec(starting_node, update_node)
+
+    def print_tree(self):
+        current_node = self.base
+        if current_node:
+            print(current_node.value)
+            return self.print_tree(current_node.right_b)
+            return self.print_tree(current_node.left_b)
+        else:
+            return
--- python/data_structures/bi_tree_test.py
+++ python/data_structures/bi_tree_test.py
@@ -5,7 +5,7 @@ from random import shuffle
 
 @pytest.fixture
 def random_array():
-    randomized_arr = list(range(1, 11))
+    randomized_arr = list(range(1, 21))
     shuffle(randomized_arr)
     return randomized_arr
 
@@ -32,8 +32,51 @@ def test_can_search_values_in_binaryTree():
     for item in rand_arr:
         bi_tree.insert(item)
 
-    assert bi_tree.node_count == 10
+    assert bi_tree.node_count == 20
     assert bi_tree.search(1) is True
     assert bi_tree.search(2) is True
     assert bi_tree.search(8) is True
     assert bi_tree.search(33) is False
+
+def test_remove_from_single_node_bitree():
+    # test single value tree removal
+    bi_tree = BinaryTree()
+    bi_tree.insert(4)
+    assert bi_tree.remove(4)
+    assert bi_tree.search(4) is False
+
+def test_remove_from_simple_bi_tree():
+    # test multi-value tree removal
+    bi_tree = BinaryTree(4)
+    bi_tree.insert(3)
+    bi_tree.insert(5)
+    bi_tree.remove(4)
+    bi_tree.search(4) is False
+    bi_tree.search(3) is True
+    bi_tree.search(5) is True
+
+def test_return_node():
+    rand_arr = random_array()
+    bi_tree = BinaryTree()
+    for num in rand_arr:
+        bi_tree.insert(num)
+    bi_tree.search(5) is True
+    bi_tree.return_node(5).value == 5
+    bi_tree.return_node(10).value == 10
+    bi_tree.return_node(7).value == 7
+    bi_tree.return_node(1).value == 1
+    bi_tree.return_node(11).value == 11
+    bi_tree.return_node(8).value == 8
+
+
+def test_remove_from_full_bi_tree():
+    bi_tree = BinaryTree()
+    rand_arr = random_array()
+    for num in rand_arr:
+        bi_tree.insert(num)
+    assert bi_tree.search(5) is True
+    bi_tree.remove(5)
+    assert bi_tree.search(5) is False
+    for num in rand_arr:
+        if num != 5:
+            assert bi_tree.search(num) is True
